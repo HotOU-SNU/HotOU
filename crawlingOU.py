ed0fde3f7cbb56aec7960326d0683f7a17ce0669
mscha <mscha@altair.snu.ac.kr>
2016-12-15 16:07:07 +0900

    add KEYINTERRUPT

--- crawlingOU.py
+++ crawlingOU.py
@@ -10,8 +10,9 @@ boards = {
     'sisa': [643000, 814061],
     'humordata': [1491211, 1691212],
     'freeboard': [1221155, 1437159],
-    'star': [343816, 388818],
-    'beauty': [40137, 92137]
+    'star': [343816, 308818],
+    'beauty': [40137, 92137],
+    'bestofbest':[227000, 290075]
 }    
 
 #article_no = np.random.randint(643000, 814061, size=10000)
@@ -35,28 +36,31 @@ while i < noArticle:
     #url = 'http://www.todayhumor.co.kr/board/view.php?table={}&no={}'.format(board, no)
     url = 'http://m.todayhumor.co.kr/view.php?table={}&no={}'.format(board,no)
     try:
-        soup = getSoup(url, 'html.parser')
-        if '불편을 드려서' in soup.text:
-        	print('banned from OU')
-        	break
-        #isArticle = soup.find_all('title')
-        #if '현재 블라인드 상태인 게시물입니다.' in soup.text:
-        #	print('blined article')
-        #	continue
-        #if '해당 게시물이 존재하지 않습니다' in soup.text:
-        #	print('deleted article')
-        #	continue
-        article_infos.append(getArticleInfos_m(soup))
-    except urllib.error.URLError:
-        print("Timeout : Server can't respond")
+      soup = getSoup(url, 'html.parser')
+      if '불편을 드려서' in soup.text:
+              print('banned from OU')
+              break
+      #isArticle = soup.find_all('title')
+      #if '현재 블라인드 상태인 게시물입니다.' in soup.text:
+      #	print('blined article')
+      #	continue
+      #if '해당 게시물이 존재하지 않습니다' in soup.text:
+      #	print('deleted article')
+      #	continue
+      article_infos.append(getArticleInfos_m(soup))
+      time.sleep(2)
+    except KeyboardInterrupt :
+        print("KEYBOARDINTERRUPT : break loop")
         break
+    except urllib.error.URLError:
+      print("Timeout : Server can't respond")
+      break
     except Exception as e:
-        print(no)
-        continue
+      print(no)
+      continue
     end = time.time()
     print('[%5d]: %d took %f'%(i,no,end-start))
     i = i + 1
-    time.sleep(2)
 
 with open('{}_db.pkl'.format(board), 'wb') as f:
     pkl.dump(article_infos, f)
